a02c81c9e2fbb3520a7d08e9076157b863d2f5a4
David Emmert <emmertd30@gmail.com>
2024-09-11 14:04:54 -0400

    Added code for standard enemy

--- PP2 FPS Game Team Version/Assets/Scripts/EnemyType1.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/EnemyType1.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyType1 : BasicEnemyAI
 {
+
+    [SerializeField] int HP;
+
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform headPos;
+
+    [SerializeField] float attackRate;
+    [SerializeField] int difficulty;
+    [SerializeField] int faceTargetSpeed;
+    Color colorOrig;
+
+    bool isAttacking;
+    bool playerInRange;
+
+    Vector3 playerDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        colorOrig = model.material.color;
+        gameManager.instance.UpdateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //player position needs to be updated to the AI
+        if (playerInRange)
+        {
+            playerDir = gameManager.instance.player.transform.position - headPos.position;
+            agent.SetDestination(gameManager.instance.player.transform.position);
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+
+        }
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+    void FaceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    IEnumerator attack()
+    {
+        isAttacking = true;
+
+        yield return new WaitForSeconds(attackRate);
+        isAttacking = false;
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+
+        HP -= amount;
+        StartCoroutine(FlashColor());
+        if (HP <= 0)
+        {
+            gameManager.instance.UpdateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator FlashColor()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+
 }
