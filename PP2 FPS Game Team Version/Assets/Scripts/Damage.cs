49894b7e281bd8d72408c0b90ba703f906740ce1
Max <166408857+LeGermen@users.noreply.github.com>
2024-09-11 23:22:45 -0400

    FInished with the models and added functionality to the fireball

--- PP2 FPS Game Team Version/Assets/Scripts/Damage.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Damage.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    // Type of damage.
+    [SerializeField] enum damageType { Magic, Ranged, Melee, Enviromental }
+    [SerializeField] damageType type;
+    [SerializeField] Rigidbody rigidBody;
+
     // Amount of damage to apply to the player.
-    [SerializeField] public float damageAmount = 1f;
+    [SerializeField] float damageAmount = 1f;
+
+    // In case of ranged object
+    [SerializeField] float speed;
+    [SerializeField] float destroyTime;
+
+    // Getters
+    public float DestroySpeed { get { return destroyTime; } }
+    public float Speed { get { return speed; } }
+    public Rigidbody RigidBody { get { return rigidBody; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (type == damageType.Ranged)
+        {
+            rigidBody.velocity = transform.forward * speed;
+            Destroy(gameObject, destroyTime);
+        }
+    }
 
     // Detect player collision.
     private void OnTriggerEnter(Collider other)
@@ -15,9 +40,10 @@ public class Damage : MonoBehaviour
 
         if (other.CompareTag("Player"))
         {
+            // Do the damage.
             other.GetComponent<FirstPersonController>().TakeDamage(damageAmount);
+            Destroy(gameObject);
         }
-
     }
 
 }
---
+++ PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/FollowProjectile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowProjectile : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float rotSpeed;
+    [SerializeField] float speed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = gameManager.instance.player.transform;
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - rb.position;
+        Vector3 rot = Vector3.Cross(transform.forward, dir);
+        rb.angularVelocity = rot * rotSpeed;
+        rb.velocity = transform.forward * speed;
+    }
+}
