7c0cab0e886d3e5e6055e04783113f3fc4abfb6b
Max <166408857+LeGermen@users.noreply.github.com>
2024-09-19 15:31:43 -0400

    Basic Animations for Enemy Type 1

--- PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/EnemyType1.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/EnemyType1.cs
@@ -8,42 +8,61 @@ public class EnemyType1 : BasicEnemyAI
 {
 
     [SerializeField] float attackRate;
-    bool isAttacking;
     [SerializeField] float meleeRange;
+    [SerializeField] int animSpeedTrans;
+    bool isAttacking;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        NavMeshAgent agent = GetAgent();
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        float animSpeed = anim.GetFloat("Speed");
+        anim.SetFloat("Speed", Mathf.Lerp(animSpeed, agentSpeed, Time.deltaTime * animSpeedTrans));
         Movement();
-        //player position needs to be updated to the AI
+
+        // Checks is player is in attack range
         if (GetPlayerInRange())
         {
+            // Calculates the distance between the enemy and the player
             float distance = Vector3.Distance(transform.position,gameManager.instance.player.transform.position);
-            if (distance <= meleeRange) {
-                if (!isAttacking) {
+
+            if (distance <= meleeRange) 
+            {
+                // In attack range
+                if (!isAttacking) 
+                {
                     StartCoroutine(attack());
                 }
-                
-            }
 
+            }
+        }
+
+        if (GetHP() <= 0)
+        {
+            agent.velocity = Vector3.zero;
+            SetPlayerInRange(false);
+            SetFaceTargetSpeed(0);
+            anim.SetTrigger("Death");
         }
     }
     IEnumerator attack()
     {
         isAttacking = true;
-        gameManager.instance.playerScript.currentHealth--;
+        anim.SetTrigger("Attack");
         yield return new WaitForSeconds(attackRate);
         isAttacking = false;
-
     }
 
-
-
-
+    public void DoDamage()
+    {
+        gameManager.instance.playerScript.currentHealth--;
+    }
 }
