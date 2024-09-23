34e95be54a2dbb1009aaec9cfd3c12113bad0c24
William Ponzo <wtponzo@student.fullsail.edu>
2024-09-23 09:55:05 -0400

    Weapon Anim + Scripts Rework
    Trying to get weapon functionality

---
+++ PP2 FPS Game Team Version/Assets/Scripts/Weapons/Sword.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Weapon
+{
+    // Sword Paramaters
+    private Animator swordAnimator;
+    private bool isEquipped = false;
+    private bool canAttack = true;
+    private bool isAttacking = false;
+    public float attackCooldown = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponName = "Sword";
+        swordAnimator = GetComponent<Animator>();
+
+        // Check if the sword is equipped at the start
+        if (transform.parent != null)
+        {
+            isEquipped = true;
+            canAttack = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEquipped && canAttack && Input.GetMouseButtonDown(0))
+            TriggerSwordAttack();
+    }
+
+    public override void Pickup(Transform parentTransform)
+    {
+        base.Pickup(parentTransform);
+        isEquipped = true;
+        canAttack = true;
+        Debug.Log("Sword is equipped.");
+    }
+
+    public override void Drop()
+    {
+        if (!isAttacking)
+        {
+            base.Drop();
+            isEquipped = false;
+            Debug.Log("Sword dropped.");
+        }
+        else
+        {
+            Debug.Log("Cannot drop the sword while attacking!");
+        }
+
+    }
+
+    public void TriggerSwordAttack()
+    {
+        if (swordAnimator != null && canAttack)
+        {
+            // Trigger the sword swing animation
+            swordAnimator.SetTrigger("SwordAttack");
+            isAttacking = true;
+            StartCoroutine(AttackCooldown());
+        }
+        else
+        {
+            Debug.Log("No animator component found or cannot attack...");
+        }
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+
+        yield return new WaitForSeconds(attackCooldown);
+
+        while (swordAnimator.GetCurrentAnimatorStateInfo(0).IsName("SwordAttack"))
+        {
+            yield return null; // wait for swing anim to complete
+        }
+
+        isAttacking = false;
+        canAttack = true;
+        Debug.Log("Attack finished.");
+    }
+}
