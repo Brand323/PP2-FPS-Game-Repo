2ebee04dcf6db5007f5a0be203eb6948546d5591
Max <166408857+LeGermen@users.noreply.github.com>
2024-09-26 10:03:14 -0400

    Added combat functionality for the bow

--- PP2 FPS Game Team Version/Assets/Scripts/Weapons/Bow.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Weapons/Bow.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bow : Weapon
+public class Bow : BaseWeapon
 {
+    [SerializeField] GameObject arrow;
+    [SerializeField] float damageAmount;
+    [SerializeField] Animator bowAnimator;
     public Transform arrowTransform;
     public Camera playerCam;
     public float maxAimDist = 100f;
 
-    private Animator bowAnimator;
-    private bool isEquipped = false;
     private bool canAttack = true;
     private bool isDrawingArrow = false;
     private bool bowIsFullyDrawn = false;
@@ -24,13 +25,13 @@ public class Bow : Weapon
     // Start is called before the first frame update
     void Start()
     {
+        base.Start();
+
         weaponName = "Bow";
-        bowAnimator = GetComponent<Animator>();
         
 
         if (transform.parent != null)
         {
-            isEquipped = true;
             canAttack = true;
         }
         
@@ -39,8 +40,9 @@ public class Bow : Weapon
     // Update is called once per frame
     void Update()
     {
+        base.Update();
+
         HandleBowInput();
-        UpdateArrowAim();
     }
 
     public void HandleBowInput()
@@ -48,16 +50,31 @@ public class Bow : Weapon
         if (!canAttack)
             return;
 
+        // Right click will start drawing the bow
         if (Input.GetMouseButtonDown(1) && !isDrawingArrow)
             StartDrawingBow();
-
+        // If right click is lifted before the bow is fully drawn it will cancel the animation
         if (Input.GetMouseButtonUp(1) && isDrawingArrow && !bowIsFullyDrawn)
             CancelBowDraw();
-
+        // Left click while bow is fully drawn will shoot the arrow
         if (Input.GetMouseButtonDown(0) && bowIsFullyDrawn)
             ShootBow();
     }
 
+    protected override void DropWeapon()
+    {
+        if (!isDrawingArrow && !bowIsFullyDrawn)
+        {
+            base.DropWeapon();
+            isEquipped = false;
+            Debug.Log("Bow dropped.");
+        }
+        else
+        {
+            Debug.Log("Cannot drop the bow while drawing!");
+        }
+
+    }
     public void StartDrawingBow()
     {
         isDrawingArrow = true;
@@ -76,48 +93,91 @@ public class Bow : Weapon
 
     public void CancelBowDraw()
     {
+        // Cancels the Draw Animation
         if (drawCoroutine != null)
         {
             StopCoroutine(drawCoroutine);
         }
         bowIsFullyDrawn = false;
         isDrawingArrow = false;
+
+        // Updates the animation
         bowAnimator.SetBool("IsDrawing", false);
         Debug.Log("Draw canceled.");
     }
 
     public void ShootBow()
     {
+        // Updates the animation
         bowAnimator.SetBool("IsDrawing", false);
         bowAnimator.SetTrigger("Shoot");
+
         bowShot = true;
         canAttack = false;
         isDrawingArrow = false;
         bowIsFullyDrawn = false;
+
+        // Shoots the arrow
+        ShootArrow();
 
         StartCoroutine(ResetAfterCooldown());
     }
 
     private IEnumerator ResetAfterCooldown()
     {
+        // Cooldown to shoot again
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
         bowIsFullyDrawn = false;
         Debug.Log("Bow Attack Ready.");
     }
 
-    private void UpdateArrowAim()
+    protected override string GetWeaponName()
     {
+        // Returns the name of the object
+        return "Bow";
+    }
+
+    void ShootArrow()
+    {
+        // Aim the arrow towards the target
         Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
-
         Vector3 aimPoint;
 
+        // Determine where the player is aiming (either hit point or far maxAimDist)
         if (Physics.Raycast(ray, out hit, maxAimDist))
             aimPoint = hit.point;
         else
             aimPoint = ray.GetPoint(maxAimDist);
 
-        arrowTransform.LookAt(aimPoint);
+        // Instantiate the arrow at the arrowTransform position with no initial rotation
+        GameObject firedArrow = Instantiate(arrow, arrowTransform.position, Quaternion.identity);
+
+        // Calculate the direction the arrow should face (from arrowTransform to aimPoint)
+        Vector3 direction = (aimPoint - firedArrow.transform.position).normalized;
+
+        // Ensure the arrow looks in the direction of the aim point using Quaternion.LookRotation
+        firedArrow.transform.rotation = Quaternion.LookRotation(direction);
+
+        // Pass the damage to the arrow script
+        Arrow arrowScript = firedArrow.GetComponent<Arrow>();
+        if (arrowScript == null)
+        {
+            Debug.LogError("Arrow script is missing on the arrow prefab!");
+            return;
+        }
+        arrowScript.damageAmount = damageAmount;
+
+        // Apply an initial force or velocity to the arrow to make it move forward
+        Rigidbody arrowRb = firedArrow.GetComponent<Rigidbody>();
+        if (arrowRb == null)
+        {
+            Debug.LogError("Rigidbody is missing on the arrow prefab!");
+            return;
+        }
+
+        // Apply velocity to the arrow to launch it in the direction it is facing
+        arrowRb.velocity = direction * arrowScript.speed;
     }
 }
