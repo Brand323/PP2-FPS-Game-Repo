ff5af29b8120a8c86c5990572e85535ef6c3b09d
BWKing16FS <adholt@student.fullsail.edu>
2024-09-25 20:11:08 -0400

    Made potions give either stats or stamina / Added prefabs!

--- PP2 FPS Game Team Version/Assets/Scripts/Potions/PotionManager.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Potions/PotionManager.cs
@@ -8,6 +8,33 @@ public class PotionManager : MonoBehaviour
     private int healthPotionCount = 0;
     private int staminaPotionCount = 0;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            UsePotion("Health");
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            UsePotion("Stamina");
+        }
+    }
+    void UsePotion(string type)
+    {
+        if (type == "Health" && healthPotionCount > 0)
+        {
+            gameManager.instance.playerScript.AddHealth(25);
+            healthPotionCount--;
+            UIManager.instance.healthPotionText.text = healthPotionCount.ToString();
+        }
+        else if (type == "Stamina" && staminaPotionCount > 0)
+        {
+            gameManager.instance.playerScript.AddStamina(25);
+            staminaPotionCount--;
+            UIManager.instance.staminaPotionText.text = staminaPotionCount.ToString();
+        }
+    }
+
     public int GetPotionCount(string type)
     {
         int potionNum = 0;
