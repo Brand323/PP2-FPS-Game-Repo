768f690fc70204fc18ea7409659063c773e1b0e0
Brandon Grasso <grassobrandon323@gmail.com>
2024-10-02 18:48:49 -0400

    Enemies now spawn and kinda roam around cities

--- PP2 FPS Game Team Version/Assets/Scripts/Map/MapCity.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Map/MapCity.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapCity : MonoBehaviour
 {
+    public GameObject enemyPrefab;
+    public GameObject cityPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemy();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnEnemy()
     {
+        GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
         
+        MapEnemyAi enemyScript = enemy.GetComponent<MapEnemyAi>();
+        if (enemyScript != null )
+        {
+            enemyScript.SetHomeBase(transform);
+        }
     }
 }
--- PP2 FPS Game Team Version/Assets/Scripts/Map/MapEnemyAi.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Map/MapEnemyAi.cs
@@ -7,9 +7,10 @@ public class MapEnemyAi : MonoBehaviour
 {
 
     [Header("Roaming Settings")]
-    [SerializeField] float roamRadius = 1f;
-    [SerializeField] float roamTime = 1f;
-    [SerializeField] float returnSpeed = 1f;
+    [SerializeField] float roamRadius = .4f;
+    [SerializeField] float roamTime = .4f;
+    [SerializeField] float returnSpeed = 20f;
+    [SerializeField] float roamSpeed = 20f;
 
     private NavMeshAgent agent;
     private Transform homeBase;
@@ -20,12 +21,26 @@ public class MapEnemyAi : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         roamTimer = roamTime;
+
+        if (agent != null)
+        {
+            agent.speed = roamSpeed;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        roamTimer -= Time.deltaTime;
+
+        if(roamTimer >0 )
+        {
+            Roam();
+        }
+        else
+        {
+            ReturnHome();
+        }
     }
 
     //void RoamingBrain()
@@ -48,21 +63,26 @@ public class MapEnemyAi : MonoBehaviour
             Vector3 randomDirection = Random.insideUnitSphere * roamRadius;
             randomDirection += transform.position;
 
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
+            Vector3 finalPosition = hit.position;
 
+            agent.SetDestination(finalPosition);
 
         }
     }
 
     void ReturnHome()
     {
-        
+        agent.speed = returnSpeed;
+        agent.SetDestination(homeBase.position);
+
+        if(Vector3.Distance(transform.position, homeBase.position) < 1f)
+        {
+            roamTimer = roamTime;
+            agent.speed = 1.5f;
+        }
     }
-
-
-
-
-
-
 
 
     public void SetHomeBase(Transform home)
