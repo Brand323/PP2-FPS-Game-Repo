4cd3fd05401161aadb68b6b2e0f4d420d300a83d
William Ponzo <wtponzo@student.fullsail.edu>
2024-10-02 14:09:37 -0400

    Player Weapon Manager Implemented + Animations
    Player weapons have full functionality plus animations for both the bow and the shield, and the weapon manager added to the player prefab.

--- PP2 FPS Game Team Version/Assets/Scripts/Interfaces/InteractionManager.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Interfaces/InteractionManager.cs
@@ -35,77 +35,77 @@ public class InteractionManager : MonoBehaviour
             Debug.LogError("gameManagerInstance is null!");
             return;
         }
-        HandleRayCast();
+        //HandleRayCast();
     }
 
     
-    void HandleRayCast()
-    {
-        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-        RaycastHit hit;
+    //void HandleRayCast()
+    //{
+    //    Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+    //    RaycastHit hit;
 
-        Debug.DrawRay(ray.origin, ray.direction * interactDistance, Color.green);
+    //    Debug.DrawRay(ray.origin, ray.direction * interactDistance, Color.green);
 
-        //Checks is ray hit 
-        if (Physics.Raycast(ray, out hit, interactDistance) ) 
-        {
+    //    //Checks is ray hit 
+    //    if (Physics.Raycast(ray, out hit, interactDistance) ) 
+    //    {
 
-        I_Interactable interactable = hit.collider.GetComponent<I_Interactable>();
-        BaseWeapon weapon = interactable as BaseWeapon;
+    //    I_Interactable interactable = hit.collider.GetComponent<I_Interactable>();
+    //    BaseWeapon weapon = interactable as BaseWeapon;
 
-            //Checks if object is interactable
-            if (interactable != null)
-            {
-                //Makes sure its not the same object
-                if (interactable != lastInteractedObject)
-                {
-                    //Debug.Log($"Raycast hit: {hit.collider.name}");
+    //        //Checks if object is interactable
+    //        if (interactable != null)
+    //        {
+    //            //Makes sure its not the same object
+    //            if (interactable != lastInteractedObject)
+    //            {
+    //                //Debug.Log($"Raycast hit: {hit.collider.name}");
 
-                    //Updates Last Object hit
-                    lastInteractedObject = interactable;
+    //                //Updates Last Object hit
+    //                lastInteractedObject = interactable;
 
-                    if (weapon != null)
-                    {
+    //                if (weapon != null)
+    //                {
 
-                        Debug.Log($"Interacting with weapon: {weapon.GetInteractableName()}");
-                        if (!weapon.isPurchased)
-                        {
-                            gameManagerInstance.activateItemUI($"Buy {weapon.GetInteractableName()}: {weapon.weaponPrice.ToString()} Coins", UIManager.instance.itemBuyWindow);
+    //                    Debug.Log($"Interacting with weapon: {weapon.GetInteractableName()}");
+    //                    if (!weapon.isPurchased)
+    //                    {
+    //                        gameManagerInstance.activateItemUI($"Buy {weapon.GetInteractableName()}: {weapon.weaponPrice.ToString()} Coins", UIManager.instance.itemBuyWindow);
 
-                        }
-                        else if(!weapon.isEquipped)
-                        {
-                            gameManagerInstance.activateItemUI("Pick UP ", UIManager.instance.itemPickUpWindow);
+    //                    }
+    //                    else if(!weapon.isEquipped)
+    //                    {
+    //                        gameManagerInstance.activateItemUI("Pick UP ", UIManager.instance.itemPickUpWindow);
 
-                        }
-                    }
-                }
+    //                    }
+    //                }
+    //            }
 
-                if (Input.GetKeyDown(KeyCode.E) && weapon != null)
-                {
-                    if (!weapon.isPurchased)
-                    {
-                        weapon.TryPurchaseWeapon();
-                    }
-                    else
-                    {
-                        weapon.PickupWeapon();
-                    }
+    //            if (Input.GetKeyDown(KeyCode.E) && weapon != null)
+    //            {
+    //                if (!weapon.isPurchased)
+    //                {
+    //                    weapon.TryPurchaseWeapon();
+    //                }
+    //                else
+    //                {
+    //                    weapon.PickupWeapon();
+    //                }
 
-                    gameManagerInstance.deactivateItemUI();
-                }
+    //                gameManagerInstance.deactivateItemUI();
+    //            }
                
                 
-            }
-        }
-        else  
-        {
-            //Deactivates Ui if nothing hit
-            if ((lastInteractedObject != null))
-            {
-                lastInteractedObject = null;
-                gameManagerInstance.deactivateItemUI();
-            }
-        }
-    }
+    //        }
+    //    }
+    //    else  
+    //    {
+    //        //Deactivates Ui if nothing hit
+    //        if ((lastInteractedObject != null))
+    //        {
+    //            lastInteractedObject = null;
+    //            gameManagerInstance.deactivateItemUI();
+    //        }
+    //    }
+    //}
 }
