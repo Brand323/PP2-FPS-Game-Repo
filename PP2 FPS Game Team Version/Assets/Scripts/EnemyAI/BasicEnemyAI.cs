2aad1c21e47c8ad99614eb8cf50d5a1233cba444
David Emmert <emmertd30@gmail.com>
2024-10-13 18:17:30 -0400

    melee enemy works as intended, basic enemy ai fixed

--- PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/BasicEnemyAI.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/BasicEnemyAI.cs
@@ -19,8 +19,11 @@ public class BasicEnemyAI : MonoBehaviour, I_Damage
         Jelly
     }
     [SerializeField] protected Type type;
-    [Range(0, 50)][SerializeField] float HP;
-    
+    [Range(0, 50)] [SerializeField] float HP;
+    [SerializeField] SphereCollider detectionCol;
+    //lower is faster
+    [Range(.1f, 10f)] [SerializeField] float detectTime;
+    [SerializeField] float maxDetectionRange;
 
     public GameObject coinPrefab;
     public GameObject healthPotion;
@@ -31,15 +34,17 @@ public class BasicEnemyAI : MonoBehaviour, I_Damage
     protected Color colorOrig;
     protected Vector3 targetDir;
     public float stopDistOrig;
-    [Range(0, 12)] public float speedOrig;
+    public float speedOrig;
     public float angularSpeedOrig;
     public bool isAttacking;
     public float attackRate;
-   
+
+    protected bool isExpanding = false;
     public bool isEngaged;
     protected GameObject target;
 
-    public Collider detectionCol;
+
+    protected Coroutine detectionCoroutine;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,6 +53,7 @@ public class BasicEnemyAI : MonoBehaviour, I_Damage
     }
     void Update()
     {
+        
     } 
     //moves to target
     public void Movement()
@@ -76,29 +82,34 @@ public class BasicEnemyAI : MonoBehaviour, I_Damage
         {
             targetInRange = true;
             target = other.gameObject;
+            if (detectionCoroutine != null)
+            {
+                StopCoroutine(detectionCoroutine);
+                isExpanding = false;
+            }
+            detectionCol.radius = 1f;
         }
         else
         {
             Debug.Log("Not a target: " + other.name);
         }
     }
-    //does not work properly for now.
-    private void OnTriggerExit(Collider other)
+/*    private void OnTriggerExit(Collider other)
     {
-        
-        if (other == detectionCol)
+        if (target != null)
         {
-            if ((other.CompareTag("Player") || other.CompareTag("Companion")) && other is SphereCollider)
+            if (distanceToTarget() >= detectionCol.radius)
             {
-                targetInRange = false;
-                if (isEngaged)
+                if ((other.CompareTag("Player") || other.CompareTag("Companion")))
                 {
-                    CombatManager.instance.attackingPlayerCurr--;
-                    isEngaged = false;
+                    targetInRange = false;
                 }
+                //StartCoroutine(detectTarget());
             }
         }
-    }
+    }*/
+
+
     //faces target if they are too close to move
     void FaceTarget()
     {
@@ -130,6 +141,42 @@ public class BasicEnemyAI : MonoBehaviour, I_Damage
     }
     void WalkBob()
     {
+
+    }
+    float distanceToTarget()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        return distance;
+    }
+    protected IEnumerator detectTargetCoroutine()
+    {
+        Debug.Log("coroutine start***");
+        if (isExpanding) {
+            yield break;
+        }
+        isExpanding = true;
+        float timeElapsed = 0f;
+        while (timeElapsed<=detectTime) {
+            detectionCol.radius = Mathf.Lerp(0.1f, maxDetectionRange, timeElapsed/detectTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        detectionCol.radius = maxDetectionRange;
+        yield return new WaitForSeconds(.1f);
+        isExpanding=false;
+        detectionCoroutine = null;
+        Debug.Log("coroutine end********");
+    }
+    protected void detectTarget()
+    {
+        if (detectionCoroutine != null)
+        {
+            StopCoroutine(detectionCoroutine);
+        }
+        if (detectionCoroutine == null)
+        {
+            detectionCoroutine = StartCoroutine(detectTargetCoroutine());
+        }
 
     }
     // Getters
--- PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/EnemyMelee.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/EnemyAI/EnemyMelee.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -15,11 +16,11 @@ public class EnemyMelee : BasicEnemyAI
     // Start is called before the first frame update
     void Start()
     {
+        CombatManager.instance.enemiesExisting++;
         colorOrig = model.material.color;
         stopDistOrig = agent.stoppingDistance;
         angularSpeedOrig = agent.angularSpeed;
         speedOrig = agent.speed;
-        detectionCol = GetComponentInChildren<SphereCollider>();
         // CombatManager.instance.enemiesExisting++;
         weaponAnimator = GetComponentInChildren<Animator>();
         if (meleeCol != null) { 
@@ -31,32 +32,52 @@ public class EnemyMelee : BasicEnemyAI
     // Update is called once per frame
     void Update()
     {
+        if (target == null||(target!=null &&!target.activeSelf)){
+            if (target != null && !target.activeSelf)
+            {
+                target=null;
+            }
+                if (detectionCoroutine == null)
+            {
+                detectTarget();
+            }
+            targetInRange = false;
+        }
         Movement();
         if (targetInRange)
         {
-            float distance = Vector3.Distance(transform.position, target.transform.position);
-            if (distance <= meleeRange)
+            if (detectionCoroutine != null)
             {
-                if (!isAttacking)
-                {
-                    StartCoroutine(attack());
-                }
+                StopCoroutine(detectionCoroutine);
+                detectionCoroutine = null;
+                isExpanding = false;
             }
-            else
+            if (target != null)
             {
-                
+                float distance = Vector3.Distance(transform.position, target.transform.position);
+                if (distance <= meleeRange)
+                {
+                    if (!isAttacking)
+                    {
+                        StartCoroutine(attack());
+                    }
+                } else
+                {
+                    meleeColOff();
+                }
+
             }
         }
     }
    private void OnTriggerEnter(Collider other)
     {
         base.OnTriggerEnter(other);
-        if (meleeCol.enabled && !hasDamaged)
+/*        if (meleeCol.enabled && !hasDamaged)
         {
 
             if (other.CompareTag("Player") || other.CompareTag("Companion"))
             {
-                I_Damage dmg = GetComponent<I_Damage>();
+                I_Damage dmg = other.GetComponent<I_Damage>();
                 if (dmg != null)
                 {
                     dmg.TakeDamage(dmgAmount);
@@ -64,10 +85,12 @@ public class EnemyMelee : BasicEnemyAI
                     meleeColOff();
                 }
             }
-        }
+        }*/
     }
     IEnumerator attack()
     {
+        hasDamaged = false;
+        meleeColOn();
         isAttacking = true;
         //weaponAnimator.SetTrigger("TestSwordAnim");
         TriggerSwordAttack();
@@ -102,6 +125,7 @@ public class EnemyMelee : BasicEnemyAI
     }
     public override void Death()
     {
+        base.Death();
         Destroy(gameObject);
     }
 
