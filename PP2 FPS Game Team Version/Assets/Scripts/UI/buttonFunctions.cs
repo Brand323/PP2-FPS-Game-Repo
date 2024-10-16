2692a40e2a40464f8dc5f27e51eafb939f965934
vhm11 <vhm110305@gmail.com>
2024-10-15 21:51:06 -0400

    Button functions fix.

--- PP2 FPS Game Team Version/Assets/Scripts/UI/buttonFunctions.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/UI/buttonFunctions.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using TMPro;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
