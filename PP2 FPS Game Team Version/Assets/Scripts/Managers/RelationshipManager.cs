ab7bc16886abe5ecb6c7588eafce8bd3d39781c0
BWKing16FS <adholt@student.fullsail.edu>
2024-10-20 13:45:06 -0400

    updated relationshipManager and relationshipData

--- PP2 FPS Game Team Version/Assets/Scripts/Managers/RelationshipManager.cs
+++ PP2 FPS Game Team Version/Assets/Scripts/Managers/RelationshipManager.cs
@@ -18,49 +18,44 @@ public class RelationshipManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        // Initialize the kingdom's economies
-        foreach (var relationship in relationships)
-        {
-            GetKingdomRelationship(relationship);
-        }
 
-        foreach (var relationship in relationships)
-        {
-            GetKingdomRelationshipRank(relationship);
-        }
     }
 
-    private void GetKingdomRelationship(RelationshipData relationship)
-    {
-        //Debug.Log($"Player has {relationship.kingdomPoints} Kingdom Points towards the {relationship.kingdomData.kingdomName} Kingdom.");
-    }
-
-    private void GetKingdomRelationshipRank(RelationshipData relationship)
+    void updateKingdomRelationshipRank(RelationshipData relationship)
     {
         int relationshipKingdomPoints = relationship.kingdomPoints;
 
-        //string relationshipRank = "";
+        string relationshipRank = "";
 
-        //// TODO: Make this more flexible.
-        //if (relationshipKingdomPoints == -100)
-        //    relationshipRank = "Betrayer of the Kingdom";
-        //if (relationshipKingdomPoints > -99 && relationshipKingdomPoints < -50)
-        //    relationshipRank = "Enemy";
-        //else if (relationshipKingdomPoints > -49 && relationshipKingdomPoints < 49)
-        //    relationshipRank = "Neutral";
-        //else if (relationshipKingdomPoints > 50 && relationshipKingdomPoints < 99)
-        //    relationshipRank = "Ally";
-        //else if (relationshipKingdomPoints == 100)
-        //    relationshipRank = "Hero of the Kingdom";
+        if (relationshipKingdomPoints == -100)
+            relationshipRank = "Betrayer of the Kingdom";
+        if (relationshipKingdomPoints > -99 && relationshipKingdomPoints < -50)
+            relationshipRank = "Enemy";
+        else if (relationshipKingdomPoints > -49 && relationshipKingdomPoints < 49)
+            relationshipRank = "Neutral";
+        else if (relationshipKingdomPoints > 50 && relationshipKingdomPoints < 99)
+            relationshipRank = "Ally";
+        else if (relationshipKingdomPoints == 100)
+            relationshipRank = "Hero of the Kingdom";
 
-        //Debug.Log($"Player is ranked {relationshipRank} to {relationship.kingdomData.kingdomName}.");
+        relationship.kingdomRank = relationshipRank;
+    }
+
+    void setKingdomPoints(int points ,RelationshipData relationship)
+    {
+        if (relationship != null)
+        {
+            relationship.kingdomPoints += points;
+            updateKingdomRelationshipRank(relationship);
+        }
     }
 
     void addKingdomPoints(int pointsToAdd, RelationshipData relationship)
     {
         if (relationship != null)
         {
-            relationship.kingdomPoints += pointsToAdd; 
+            relationship.kingdomPoints += pointsToAdd;
+            updateKingdomRelationshipRank(relationship);
         }
     }
 
@@ -69,6 +64,7 @@ public class RelationshipManager : MonoBehaviour
         if (relationship != null)
         {
             relationship.kingdomPoints -= pointsToSubtract;
+            updateKingdomRelationshipRank(relationship);
         }
     }
 }
